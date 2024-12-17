070eda13cedcb3ca90a085a5f75052b05337e68c
JJHA <herrerajose.108f@gmail.com>
2024-12-17 16:09:08 -0600

    super user and model created

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ["created", "updated"]
+
+admin.site.register(Task, TaskAdmin) # registramos la aplicaicon en el adminstrador de django
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    STATUS_CHOISE = [
+        ('PENDING', 'Pendiente'),
+        ('IN_PROGRESS', 'En Proceso'),
+        ('COMPLETED', 'Completada'),
+    ]
+
+    title = models.CharField(max_length=250)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=15, choices=STATUS_CHOISE, default='PENDING')
+    created = models.DateTimeField(auto_now_add=True)
+    updated =models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # asignamos una tarea el id del usuario creador
+
+    def __str__(self):
+        return self.title
