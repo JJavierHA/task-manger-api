8cf56e1f4b514f064d1029489101683a16e0bc4a
JJHA <herrerajose.108f@gmail.com>
2024-12-17 21:04:26 -0600

    Docker implementation

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "task_manager.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "taskdb",
+        "USER": "user",
+        "PASSWORD": "password",
+        "HOST": "db",  # Nombre del servicio en docker-compose
+        "PORT": "5432",
     }
 }
 
